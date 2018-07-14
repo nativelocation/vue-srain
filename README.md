55f6e7ca4f39a8626bf59e8a2049ae5a321f0c4a
nativelocation <bluesky6460@mail.com>
2018-07-14 13:26:50 -0500

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# rtvision-vue
+rtvision-vue
+
+### Install dependencies
+`cd` into the root of the project and run this command
+```sh
+$ npm install
+```
+
+### Run project
+```sh
+$ npm start
+```
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="app">
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+    @import '~styles/variables';
+    @import '../node_modules/bootstrap/scss/bootstrap.scss';
+</style>
